a84d782cbde54e611c24458ccf9056c8913b005b
JulianMiranda <jmirandauria@gmail.com>
2022-07-17 10:19:58 -0500

    Tabs Work

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {
   View,
   Image,
